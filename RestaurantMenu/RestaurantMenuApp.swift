9c86b458c3ce1cfa0e5f87a22c62e7e5ad9113dc
Walter Bernal <wltrbm@gmail.com>
2023-09-27 19:30:15 -0700

    Initial Commit

---
+++ RestaurantMenu/RestaurantMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantMenuApp.swift
+//  RestaurantMenu
+//
+//  Created by Walter Bernal Montero on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
