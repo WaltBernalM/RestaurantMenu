057c966d45f5c15b6c05f54856908ecb02502226
Walter Bernal <wltrbm@gmail.com>
2023-09-28 06:04:23 -0700

    Setup of all project to work as MVP

--- RestaurantMenu/ContentView.swift
+++ RestaurantMenu/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var mockData = MockData()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MenuItemsView()
         }
+        .environmentObject(mockData)
         .padding()
     }
 }
