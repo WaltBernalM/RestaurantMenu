44998a8497174df8f4ae9399a6a00efcc4dac176
Walter Bernal <wltrbm@gmail.com>
2023-09-28 19:44:24 -0700

    Creation of MenuItemProtocol.swift

--- RestaurantMenu/MenuItemProtocol.swift
+++ RestaurantMenu/MenuItemProtocol.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol MenuItemProtocol: Identifiable {
+    var id: UUID { get }
+    var price: Double { get }
+    var title: String { get }
+    var menuCategory: MenuCategory { get }
+    var ordersCount: Int { get set}
+    var ingredients: [Ingredient] {get set}
+}
