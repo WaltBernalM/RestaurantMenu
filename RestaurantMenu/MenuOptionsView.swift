8320f4829d98eaa90438b78965ae4b61580c2a78
Walter Bernal <wltrbm@gmail.com>
2023-09-28 19:46:51 -0700

    Adaptation of MenuOptionsView to MenuViewModel

--- RestaurantMenu/MenuOptionsView.swift
+++ RestaurantMenu/MenuOptionsView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct MenuOptionsView: View {
+    @EnvironmentObject var menuViewModel: MenuViewModel
     @Binding var isVisible: Bool
+    
+    @Binding var selectedCategory: SelectCriteria
+    @State private var selected: Int = -1
+    
+    @Binding var sortBy: SortCriteria
+    @State private var sort: Int = -1
+    
     var body: some View {
         VStack(alignment: .center) {
             HStack {
                 Spacer()
                 Button(action: {
+                    if selected == 0 { selectedCategory = .all }
+                    else if selected == 1 { selectedCategory = .food }
+                    else if selected == 2 { selectedCategory = .drinks }
+                    else if selected == 3 { selectedCategory = .desserts }
+                    else if selected == -1 {
+                        selectedCategory = selectedCategory
+                    }
+                    
+                    if sort == 0 {
+                        sortBy = .null
+                        menuViewModel.sortMenuItems(sortCriteria: sortBy)
+                    } else if sort == 1 {
+                        sortBy = .popular
+                        menuViewModel.sortMenuItems(sortCriteria: sortBy)
+                    } else if sort == 2 {
+                        sortBy = .price
+                        menuViewModel.sortMenuItems(sortCriteria: sortBy)
+                    } else {
+                        sortBy = .aToZ
+                        menuViewModel.sortMenuItems(sortCriteria: sortBy)
+                    }
+                    
+                    print("selectedCategory: ", selectedCategory)
+                    print("sortBY: ",sortBy)
                     isVisible.toggle()
                 }) {
                     Text("Done").fontWeight(.bold)
@@ -36,30 +68,58 @@ struct MenuOptionsView: View {
             List {
                 Section(header: Text("SELECTED CATEGORIES")) {
                     Text("Food").onTapGesture {
-                        // Something goes here
+                        if selected != 1 { selected = 1 }
+                        else { selected = 0 }
                     }
-                    
+                    .listRowBackground(
+                        selected == 1 || (selected == -1 && selectedCategory == .food)
+                        ? Color.gray
+                        : Color.white
+                    )
                     Text("Drink").onTapGesture {
-                        // Something goes here
+                        if selected != 2 { selected = 2 }
+                        else { selected = 0 }
                     }
-                    
+                    .listRowBackground(
+                        selected == 2 || (selected == -1 && selectedCategory == .drinks)
+                        ? Color.gray
+                        : Color.white
+                    )
                     Text("Dessert").onTapGesture {
-                        // Something goes here
+                        if selected != 3 { selected = 3 }
+                        else { selected = 0 }
                     }
-                    
+                    .listRowBackground(
+                        selected == 3 || (selected == -1 && selectedCategory == .desserts)
+                        ? Color.gray : Color.white
+                    )
                 }
                 Section(header: Text("SORT BY")) {
                     Text("Most Popular").onTapGesture {
-                        // Something goes here
+                        if sort != 1 { sort = 1 }
+                        else { sort = 0 }
                     }
-                    
+                    .listRowBackground(
+                        sort == 1 || (sort == -1 && sortBy == .popular)
+                        ? Color.gray : Color.white
+                    )
                     Text("Price $-$$$").onTapGesture {
-                        // Something goes here
+                        if sort != 2 { sort = 2 }
+                        else { sort = 0 }
                     }
+                    .listRowBackground(
+                        sort == 2 || (sort == -1 && sortBy == .price)
+                        ? Color.gray : Color.white
+                    )
                     
                     Text("A-Z").onTapGesture {
-                        // Something goes here
+                        if sort != 3 { sort = 3 }
+                        else { sort = 0 }
                     }
+                    .listRowBackground(
+                        sort == 3 || (sort == -1 && sortBy == .aToZ)
+                        ? Color.gray : Color.white
+                    )
                 }
             }
             .listStyle(.grouped)
@@ -71,6 +131,10 @@ struct MenuOptionsView: View {
 
 struct MenuOptionsView_Previews: PreviewProvider {
     static var previews: some View {
-        MenuOptionsView(isVisible: .constant(true))
+        MenuOptionsView(
+            isVisible: .constant(true),
+            selectedCategory: .constant(.all),
+            sortBy: .constant(.aToZ)
+        )
     }
 }
