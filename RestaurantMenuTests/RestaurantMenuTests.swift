aa4d16cf269aff4a6aad92b64ee25b1a7455191e
Walter Bernal <wltrbm@gmail.com>
2023-09-28 20:33:51 -0700

    Creation and setup of tests

--- RestaurantMenuTests/RestaurantMenuTests.swift
+++ RestaurantMenuTests/RestaurantMenuTests.swift
@@ -6,30 +6,14 @@
 //
 
 import XCTest
-
+@testable import RestaurantMenu
 final class RestaurantMenuTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_meniItemTitle_initWithTitle_menuItemTitleEqualsInitTitle() {
+        let menuItem = MenuItem(title: "Test")
+        XCTAssert(menuItem.title == "Test")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_menuItemIngredients_initWithIngredients_menuItemIgredientsEqualsInitIngredients() {
+        let menuItem = MenuItem(ingredients: [.pasta, .spinach, .tomatoSauce])
+        XCTAssert(menuItem.ingredients == [Ingredient.pasta, Ingredient.spinach, Ingredient.tomatoSauce])
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
